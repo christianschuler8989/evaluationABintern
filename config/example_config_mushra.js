ce505332a96b74207ab0059c3a3558de4e88529e
BAJUKA <pshravan.nayak.ece17@itbhu.ac.in>
2021-05-27 03:00:49 +0530

    some changes to html

--- config/example_config_mushra.js
+++ config/example_config_mushra.js
@@ -1,6 +1,6 @@
 // configure the test here
 var TestConfig = {
-  "TestName": "Mushra Demo Test",
+  "TestName": "Mushra Test for TTS outputs",
   "RateScalePng": "img/scale_abs.png",
   "RateScaleBgPng": "img/scale_abs_background.png",
   "RateMinValue": 0,
